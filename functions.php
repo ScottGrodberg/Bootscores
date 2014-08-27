c964c0bb1c769b5296be650990e623a7d2497fdb
Scott Grodberg <scott@trwebco.com>
2014-08-27 11:28:40 -0400

    Augmented the gallery generation with bootstrap grid classes

--- functions.php
+++ functions.php
@@ -150,5 +150,108 @@ function wp_bootstrap_head_cleanup() {
 	remove_action( 'wp_head', 'wlwmanifest_link' );                       // Windows Live Writer	
 	remove_action( 'wp_head', 'wp_generator' );                           // WP version
 }
-	// launching operation cleanup
-	add_action('init', 'wp_bootstrap_head_cleanup');
+// launching operation cleanup
+add_action('init', 'wp_bootstrap_head_cleanup');
+
+function my_post_gallery_bootscores( $string, $attr) {
+  global $post;
+
+	static $instance = 0;
+	$instance++;
+
+	if ( ! empty( $attr['ids'] ) ) {
+		// 'ids' is explicitly ordered, unless you specify otherwise.
+		if ( empty( $attr['orderby'] ) )
+			$attr['orderby'] = 'post__in';
+		$attr['include'] = $attr['ids'];
+	}
+
+	// We're trusting author input, so let's at least make sure it looks like a valid orderby statement
+	if ( isset( $attr['orderby'] ) ) {
+		$attr['orderby'] = sanitize_sql_orderby( $attr['orderby'] );
+		if ( !$attr['orderby'] )
+			unset( $attr['orderby'] );
+	}
+
+	extract(shortcode_atts(array(
+			'order'      => 'ASC',
+			'orderby'    => 'menu_order ID',
+			'id'         => $post->ID,
+			'itemtag'    => 'figure',
+			'icontag'    => 'div',
+			'captiontag' => 'figcaption',
+			'size'       => 'thumbnail',
+			'include'    => '',
+			'exclude'    => ''
+	), $attr));
+
+	$id = intval($id);
+	if ( 'RAND' == $order )
+			$orderby = 'none';
+
+	if ( !empty($include) ) {
+		$_attachments = get_posts( array('include' => $include, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+
+		$attachments = array();
+		foreach ( $_attachments as $key => $val ) {
+			$attachments[$val->ID] = $_attachments[$key];
+		}
+	} elseif ( !empty($exclude) ) {
+		$attachments = get_children( array('post_parent' => $id, 'exclude' => $exclude, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	} else {
+		$attachments = get_children( array('post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $order, 'orderby' => $orderby) );
+	}
+
+	if ( empty($attachments) )
+		return '';
+
+	if ( is_feed() ) {
+		$output = "\n";
+		foreach ( $attachments as $att_id => $attachment )
+			$output .= wp_get_attachment_link($att_id, $size, true) . "\n";
+		return $output;
+	}
+
+	// set default values (html5 versions)
+	$itemtag = tag_escape($itemtag);
+	$captiontag = tag_escape($captiontag);
+	$icontag = tag_escape($icontag);
+	$valid_tags = wp_kses_allowed_html( 'post' );
+	if ( ! isset( $valid_tags[ $itemtag ] ) )
+			$itemtag = 'figure';
+	if ( ! isset( $valid_tags[ $captiontag ] ) )
+			$captiontag = 'figcaption';
+	if ( ! isset( $valid_tags[ $icontag ] ) )
+			$icontag = 'div';
+
+	$selector = "gallery-{$instance}";
+	$gallery_style = $gallery_div = '';
+	if ( apply_filters( 'use_default_gallery_style', true ) )
+		$gallery_style = "";
+	$size_class = sanitize_html_class( $size );
+	$gallery_div = "<div id='$selector' class='gallery galleryid-{$id} gallery-size-{$size_class} row'>";
+	$output = apply_filters( 'gallery_style', $gallery_style . "\n\t\t" . $gallery_div );
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+		$link = isset($attr['link']) && 'file' == $attr['link'] ? wp_get_attachment_link($id, $size, false, false) : wp_get_attachment_link($id, $size, true, false);
+
+		$output .= "<{$itemtag} class='gallery-item col-xs-12 col-sm-4'>";
+		$output .= "
+				<{$icontag} class='gallery-icon'>
+						$link
+				</{$icontag}>";
+		if ( $captiontag && trim($attachment->post_excerpt) ) {
+			$output .= "
+				<{$captiontag} class='wp-caption-text gallery-caption'>
+				" . wptexturize($attachment->post_excerpt) . "
+				</{$captiontag}>";
+		}
+		$output .= "</{$itemtag}>";
+	}
+
+	$output .= "</div>\n";
+
+	return $output;
+}
+add_filter('post_gallery','my_post_gallery_bootscores',10,2);	
